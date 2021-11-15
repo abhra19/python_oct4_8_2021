80e4f5a8b7cd417fe467706c84f763d1bf6de105
Abhra Dass <abhradass@microsoft.com>
2021-11-15 15:22:34 -0500

    add Hello World

---
+++ user_tools/src/user_tools/__main__.py
@@ -0,0 +1,9 @@
+""" user tools app """     # Adding docstring module #
+
+import random
+
+print(__name__)            # Print name of module
+
+if __name__ == "__main__": # Indentation is Curly Braces for Python
+
+    print("Hello, World")
