10ebe0db2d73624cb48554c1b4eeeade6893cb87
Abhra Dass <abhradass@microsoft.com>
2021-11-15 15:47:14 -0500

    length char_pool

--- user_tools/src/user_tools/__main__.py
+++ user_tools/src/user_tools/__main__.py
@@ -2,8 +2,21 @@
 
 import random
 
-print(__name__)            # Print name of module
+if __name__ == "__main__":      # Indentation is Curly Braces for Python
 
-if __name__ == "__main__": # Indentation is Curly Braces for Python
+    password_length = int(input("Please enter a Password Length: "))
+    #print(type(password_length))   # Prints type of variable
+    print("Password Length: " + str(password_length))
 
-    print("Hello, World")
+    char_pool = []
+
+    char_pool.append('A')
+    char_pool.append('B')
+    char_pool.append('C')
+    char_pool.append('1')
+    char_pool.append('2')
+    char_pool.append('3')
+
+    print(len(char_pool))       # Length/size of char_pool
+    print(char_pool)            # Prints contents of char_pool
+    print(",".join(char_pool))  # Prints out data with "," in between
