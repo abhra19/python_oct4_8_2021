461e7e5f6dc0af9df6e597ea007878beb61cf443
Abhra Dass <abhradass@microsoft.com>
2021-11-16 00:46:58 -0500

    Random Password Generator v2

--- user_tools/src/user_tools/__main__.py
+++ user_tools/src/user_tools/__main__.py
@@ -10,21 +10,19 @@ if __name__ == "__main__":      # Indentation is Curly Braces for Python
 
     char_pool = []          # List datatype
 
-    char_pool.append('A')
-    char_pool.append('B')
-    char_pool.append('C')
-    char_pool.append('1')
-    char_pool.append('2')
-    char_pool.append('3')
+    for char_code in range(ord('A'), ord('Z') + 1): # ord returns Unicode number for a character
+        char_pool.append(chr(char_code))    # List is created having all characters from A to Z
 
-    # print(len(char_pool))       # Length/size of char_pool
-    # print(char_pool)            # Prints contents of char_pool
-    # print(",".join(char_pool))  # Prints out data with "," in between
+    for char_code in range(ord('a'), ord('z') + 1):
+        char_pool.append(chr(char_code))    # Adding a to z to char_pool list
+
+    for char_code in range(ord('0'), ord('9') + 1):
+        char_pool.append(chr(char_code))    # Adding 0 to 9 to char_pool list
+
     generated_password = []
 
     for _ in range(password_length):
         char_pool_index  = random.randrange(0,len(char_pool)) #Range value goes upto len - 1
-        #print(char_pool[char_pool_index])    
         generated_password.append(char_pool[char_pool_index]) # Each generated char stored in generated_password
 
     print("".join(generated_password))  # Another way of concat each character var
